2a3226eb39ba93fcad69c8ed09583afc3a36d7cd
fadelyking <fadely195@gmail.com>
2024-01-01 16:42:46 +0300

    login page

--- app.js
+++ app.js
@@ -24,12 +24,42 @@ const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
+passport.use(
+	new LocalStrategy(async (username, password, done) => {
+		try {
+			const user = await User.findOne({ username: username });
+			if (!user) {
+				return done(null, false, { message: "Incorrect username" });
+			}
+			if (user.password !== password) {
+				return done(null, false, { message: "Incorrect password" });
+			}
+			return done(null, user);
+		} catch (err) {
+			return done(err);
+		}
+	})
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
+
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", { user: req.user }));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
 app.post("/sign-up", async (req, res, next) => {
@@ -44,4 +74,22 @@ app.post("/sign-up", async (req, res, next) => {
 		return next(err);
 	}
 });
+
+app.post(
+	"/log-in",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/",
+	})
+);
+
+app.get("/log-out", (req, res, next) => {
+	req.logout((err) => {
+		if (err) {
+			return next(err);
+		}
+		res.redirect("/");
+	});
+});
+
 app.listen(3000, () => console.log("app listening on port 3000!"));
